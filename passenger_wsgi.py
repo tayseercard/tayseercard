0cd66d5a607dd0ed76d39e9a0b34e75fe94b494e
orafitdz <orafitdz@gmail.com>
2026-06-25 14:03:09 +0100

    8

--- passenger_wsgi.py
+++ passenger_wsgi.py
@@ -1,11 +1,20 @@
 import os
 import sys
+import django
+from django.core.wsgi import get_wsgi_application
 
-# Le chemin pointe maintenant vers repositories/tayseercard
-path = '/home/exqxfpyq/repositories/tayseercard'
-if path not in sys.path:
-    sys.path.append(path)
+# Définir la racine du projet
+project_root = os.path.dirname(os.path.abspath(__file__))
 
-os.environ['DJANGO_SETTINGS_MODULE'] = 'tayseercard.settings'
+# Ajouter le chemin pour les imports Django
+if project_root not in sys.path:
+    sys.path.insert(0, project_root)
 
-from tayseercard.wsgi import application
+# Configurer les variables d'environnement pour votre projet Django
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'tayseercard.settings')
+
+# Initialiser Django
+django.setup()
+
+# Point d'entrée pour Passenger
+application = get_wsgi_application()
